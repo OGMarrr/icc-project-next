9390d45606111f726bce36fa1bd1eb3f844c946c
OGMarrr <walohan.jaymar.bscs2019@gmail.com>
2023-04-29 00:41:49 +0800

    commitnimar

---
+++ pages/admissions.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+
+const AdmissionsPage = () => {
+
+ 
+
+  return (
+    <>
+    <div className='text-center'>
+    <h1>Admission</h1>
+    </div>
+    
+    </>
+  );
+};
+
+export default AdmissionsPage;
